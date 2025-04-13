ae5ef0d5c414b9fb6fb330762fe0f420af2489f8
Madeleine Eastwood <166740418+maddy-eastwood@users.noreply.github.com>
2025-04-13 16:57:54 -0400

    Added db generator and config instructions

--- backend/README.md
+++ backend/README.md
@@ -1,7 +1,33 @@
-`python3 -m venv .venv`
+# Welcome to Our Backend
+
+Make sure in your terminal path you have cd'd into backend directory.
+
+First create a virtual environment (you only need to do this once):
+
+`python -m venv .venv`
+
+Each time you want to use the backend, be sure to activate the virtual environment:
 
 `source .venv/bin/activate`
 
+Now you can install the dependencies for this project in your virtual environment:
+
 `pip install -r requirements.txt`
 
+With the csv files on your local computer, the code in this backend can generate the database necessary for the app. __Make sure to only run this file if food.db does NOT exist in your local backend folder.__ You only need to run this once to generate the database:
+
+`python -m app.db.db`
+
+Finally, to run our api locally:
+
 `fastapi dev app/main.py`
+
+To connect wtih front end, run:
+
+* `cd ../frontend`
+* `npm run dev`
+
+## Citation for dataset used to build the database
+U.S. Department of Agriculture (USDA), Agricultural Research Service. FoodData
+Central: USDA Global Branded Food Products Database. Version Current: April 2024.
+Internet: fdc.nal.usda.gov.
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,8 @@
 from typing import Union
-
+from app.db.db import populate_hash_table
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from thefuzz import fuzz, process
 
 app = FastAPI()
 
@@ -17,14 +18,36 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+hash_table, description_to_id_map, all_brands = populate_hash_table()
+description_choices = list(description_to_id_map.keys())
+brand_choices = list(all_brands)
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-@app.get("/quadratic")
-def read_root():
-    hashmap.quadratic(...)
-    return ...
+@app.get("/example/{key}")
+def read_root(key: int):
+    return {"result": hash_table.has(key)}
+
+@app.get("/product/{description}?brand={brand}")
+def find_products(description: str, brand: str = None):
+    # fuzzy search
+
+    # brand_matches = process.extract(brand, brand_choices, limit=25, scorer=fuzz.token_sort_ratio)
+    # best_brand_match = brand_matches[0][0]
+
+    # matched_descriptions = []
+    # for description, product in description_to_id_map.items():
+    #     for brand, product_id in product:
+    #         if brand.lower() == best_brand_match.lower():
+    #             matched_descriptions.append((description, product_id))
+    # description_result = [desc for desc, _ in matched_descriptions]
+    # desc_matches = process.extract(description, description_result, limit=25, scorer=fuzz.partial_token_sort_ratio)
+
+    result = process.extract(description, description_choices, limit=25, scorer=fuzz.partial_token_sort_ratio)
+    return {"products": [result]}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
